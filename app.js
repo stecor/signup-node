18e01d4d62cc25930625f961b8494d717db42454
Stefano Corra <stefano.corra@hotmail.com>
2022-12-19 20:19:03 -0500

    first commit

--- app.js
+++ app.js
@@ -5,7 +5,16 @@ const port = 3000
 const app = express()
 
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => res.sendFile(__dirname + '/signup.html'))
+
+app.post('/', function (req, res) {
+  var firstName = req.body.fName
+  var lastName = req.body.lName
+  var email = req.body.email
+
+  console.log(firstName, lastName, email)
+})
 
 app.listen(port, () => console.log(`Server listening on port ${port}!`))
