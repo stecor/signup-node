b664dca5bcce5d6353a0a6066e7ff1d3d7c3e22a
Stefano Corra <stefano.corra@hotmail.com>
2022-12-23 02:30:55 -0500

    setup id

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
+const { Http2ServerRequest } = require('http2')
 const port = 3000
 const app = express()
 require('dotenv').config()
 const apiKey = process.env.KEY
 const listID = process.env.ID
+const https = require('https')
 
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -13,11 +15,25 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.get('/', (req, res) => res.sendFile(__dirname + '/signup.html'))
 
 app.post('/', function (req, res) {
-  var firstName = req.body.fName
-  var lastName = req.body.lName
-  var email = req.body.email
+  const firstName = req.body.fName
+  const lastName = req.body.lName
+  const email = req.body.email
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: 'subscribed',
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName,
+        },
+      },
+    ],
+  }
 
-  console.log(firstName, lastName, email)
+  const jsonData = JSON.stringify(data)
+
+  //console.log(firstName, lastName, email)
 })
 
 app.listen(port, () => console.log(`Server listening on port ${port}!`))
